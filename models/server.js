0d1a1bac1e0aef48a7ced5eed33abe72c770e77e
Fahd A. Alotaibi <faotaibi@thiqah.sa>
2024-01-15 11:55:26 +0300

    add project

--- models/server.js
+++ models/server.js
@@ -1,13 +1,28 @@
 'use strict';
+var db = require('../models/index');
+
 module.exports = (sequelize, DataTypes) => {
-    var Server = sequelize.define('Server', {
+    const Server = sequelize.define('Server', {
         hostname: DataTypes.STRING,
         ip: DataTypes.STRING,
         user: DataTypes.STRING,
         password: DataTypes.STRING,
+        project: {
+            type: DataTypes.INTEGER,
+
+            references: {
+                // This is a reference to another model
+                model: db.Project,
+
+                // This is the column name of the referenced model
+                key: 'id',
+            }
+        },
         env: DataTypes.STRING
 
     }, {});
+
+
     Server.associate = function (models) {
         // associations can be defined here
     };
